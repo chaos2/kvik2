6f1cfac5cc64f62e05c2cf6cefde6d4768bfae92
chaos <yume_@live.com>
2013-02-10 23:11:20 +0300

    synergy no design

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Kvik2::Application.config.secret_token = 'f662aa5969ff275f6b6b9c45f159adec4f90c640e070ceb8f006d90b6ae84724c821e7b63056e28552a8d5f7a978aa5eec228314bcc198c8ba38a361d141463c'
